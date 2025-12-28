6ac0b2d1304049c61f1ce470e330e45bf9695b8b
Anderson Junior Rodrigues <andersonkz@ymail.com>
2025-12-28 15:24:39 -0300

    feat: criando metodo na classe Triangulo

--- POO-Class-Triangulo-Example/Triangle.cs
+++ POO-Class-Triangulo-Example/Triangle.cs
@@ -5,5 +5,13 @@
         public double A;
         public double B;
         public double C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            double area = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+
+            return area;
+        }
     }
 }
