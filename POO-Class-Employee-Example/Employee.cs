9b0cf26082a18c1ecf8131a22d2a5d723af435cb
Anderson Junior Rodrigues <andersonkz@ymail.com>
2025-12-28 14:51:51 -0300

    feat: poo classe empregado exemplo

---
+++ POO-Class-Employee-Example/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace POO_Class_Employee_Example
+{
+    public class Employee
+    {
+        public string Name { get; set; } = string.Empty;
+        public double Salary { get; set; }
+    }
+}
---
+++ POO-Class-Employee-Example/Program.cs
@@ -0,0 +1,21 @@
+﻿using POO_Class_Employee_Example;
+using System.Globalization;
+
+var employee1 = new Employee();
+var employee2 = new Employee();
+
+Console.WriteLine("Dados do primeiro funcionário: ");
+Console.Write("Nome: ");
+employee1.Name = Console.ReadLine();
+Console.Write("Salário: ");
+employee1.Salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Dados do segundo funcionário: ");
+Console.Write("Nome: ");
+employee2.Name = Console.ReadLine();
+Console.Write("Salário: ");
+employee2.Salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+var average = (employee1.Salary + employee2.Salary) / 2.0;
+
+Console.WriteLine($"Salário médio: {average.ToString("F2", CultureInfo.InvariantCulture)}");
