4df5985b29f74f82904c17de179a9ba667495557
Anderson Junior Rodrigues <andersonkz@ymail.com>
2025-12-28 20:30:56 -0300

    feat: exemplo poo classe retangulo

---
+++ POO-Class-Retangle-Example/Program.cs
@@ -0,0 +1,16 @@
+﻿using POO_Class_Retangle_Example;
+using System.Globalization;
+
+var rectangle = new Rectangle();
+
+Console.WriteLine("Entre com a largura e altura do retangulo: ");
+rectangle.Width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+rectangle.Height = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+var area = rectangle.Area();
+var perimeter  = rectangle.Perimeter();
+var diagonal = rectangle.Diagonal();
+
+Console.WriteLine($"AREA: {area.ToString("F2", CultureInfo.InvariantCulture)}");
+Console.WriteLine($"PERIMETRO: {perimeter.ToString("F2", CultureInfo.InvariantCulture)}");
+Console.WriteLine($"DIAGONAL: {diagonal.ToString("F2", CultureInfo.InvariantCulture)}");
---
+++ POO-Class-Retangle-Example/Rectangle.cs
@@ -0,0 +1,14 @@
+﻿namespace POO_Class_Retangle_Example
+{
+    public class Rectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public double Area() => Width * Height;
+
+        public double Perimeter() => (Width + Height) * 2;
+
+        public double Diagonal() => Math.Sqrt(Math.Pow(Width, 2) + Math.Pow(Height, 2));
+    }
+}
